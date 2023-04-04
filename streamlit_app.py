f35f384a783c3b4558ef1f89121ae6f99c8c95f0
unknown <louiecervantes@wvsu.edu.ph>
2023-04-04 16:10:13 +0800

    save changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ def app():
     
     if st.button('Show Licensure Exam Report'):
         fig = plt.figure(figsize = (10, 3))
-        sns.barplot(x = df[Exam], y = df['Passing Rate'], palette= 'viridis')
+        sns.barplot(x = df['Exam'], y = df['Passing Rate'], palette= 'viridis')
         st.pyplot(fig)
         s = 'Year: ' + year
         st.write(s)
